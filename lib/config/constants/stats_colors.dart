82116e3aa586f54664840e274c53c9404c18bfc9
Leonardo Dias <leo.lbdias@gmail.com>
2021-09-29 18:29:23 -0300

    :sparkles: (PokemonDetailsStats) adding colors

---
+++ lib/config/constants/stats_colors.dart
@@ -0,0 +1,38 @@
+Map<String, Map<String, String>> statsColors = {
+  'attack': {
+    'defaultColor': '#F08030',
+    'lightColor': '#F5AC78',
+    'darkColor': '#9C531F',
+  },
+  'defense': {
+    'defaultColor': '#F8D030',
+    'lightColor': '#FAE078',
+    'darkColor': '#A1871F',
+  },
+  'hp': {
+    'defaultColor': '#FF0000',
+    'lightColor': '#FF5959',
+    'darkColor': '#A60000',
+  },
+  'special-attack': {
+    'defaultColor': '#6890F0',
+    'lightColor': '#9DB7F5',
+    'darkColor': '#445E9C',
+  },
+  'special-defense': {
+    'defaultColor': '#78C850',
+    'lightColor': '#A7DB8D',
+    'darkColor': '#4E8234',
+  },
+  'speed': {
+    'defaultColor': '#F85888',
+    'lightColor': '#FA92B2',
+    'darkColor': '#A13959',
+  },
+};
+
+Map<String, String> defaultColor = {
+  'defaultColor': '#B0B0B0',
+  'lightColor': '#B0B0B0',
+  'darkColor': '#B0B0B0',
+};
